18314b9862e817ad1d617963f63833a400cbb2c7
ctrejom <ctrejom@unah.hn>
2024-11-16 22:59:49 -0600

    Creacion de Controllers, Config y Routes

--- Tarea2.2/index.js
+++ Tarea2.2/index.js
@@ -4,6 +4,10 @@ const app = express()
 
 const PORT = Process.env.PORT || 3000
 
+app.get('/products', (req, res) => {
+    res.send('Hola')
+})
+
 app.get('/', (req, res) => {
     res.send('Hola')
 })
